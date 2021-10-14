0d2fbb1576cfa61bf713f11ff0fd79fb32c8e67e
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-14 10:32:52 +0300

    add task, done tasks line-through

--- src/App.js
+++ src/App.js
@@ -4,21 +4,28 @@ import React, { Component } from 'react';
 export class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {tasks : [
-      { task: "Go to shopping", isDone: false },
-      { task: "Do homework", isDone: true }
-    ]};
+    this.state = {
+      tasks: [
+        { task: "Go to shopping", isDone: false },
+        { task: "Do homework", isDone: true }
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
         <h1>My Todos</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input name="task" type="text" placeholder="Do something.." required />
+          <button>Add</button>
+        </form>
         <ul className="todos-list">
-          { this.state.tasks.map((value, index) => 
-            <li key={index}>
-              <input type="checkbox" checked={value.isDone} onChange={(e) => this.updateTodoStatus(e, index)} />
-              {value.task}
+          {this.state.tasks.map((value, index) =>
+            <li key={index} className={value.isDone ? "done":"undone"}>
+              <input type="checkbox" checked={value.isDone}
+                onChange={(e) => this.updateTodoStatus(e, index)} />
+              <span>{value.task}</span>
             </li>
           )}
         </ul>
@@ -26,11 +33,21 @@ export class App extends Component {
     );
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    const newTask = { task: e.target.task.value, isDone: false };
+    const tasks = [...this.state.tasks, newTask];
+    this.setState({ tasks: tasks });
+    e.target.reset();
+  }
+
   updateTodoStatus(e, index) {
+    // console.log(index + ". eleman değişti. Yeni seçililik durumu: " + e.target.checked);
     const newTodos = [...this.state.tasks]; // mevcut dizinin kopyası
     newTodos[index].isDone = e.target.checked;
-    this.setState({task: newTodos});
+    this.setState({ task: newTodos });
   }
 }
 
 export default App;
+
