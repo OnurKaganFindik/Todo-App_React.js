77f9af83e85e6fda463ae011cd157d6d90133596
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-10-14 13:03:27 +0300

    add task and sort

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import React, { Component } from 'react';
 
 export class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -11,7 +13,6 @@ export class App extends Component {
       ]
     };
   }
-
   render() {
     return (
       <div className="App">
@@ -21,16 +22,26 @@ export class App extends Component {
           <button>Add</button>
         </form>
         <ul className="todos-list">
-          {this.state.tasks.map((value, index) =>
-            <li key={index} className={value.isDone ? "done":"undone"}>
+          {this.state.tasks
+            .sort((a,b) => a.isDone == b.isDone ? a.task.localeCompare(b.task) : a.isDone - b.isDone)
+            .map((value, index) =>
+            <li key={index} className={"task-list-item " + (value.isDone ? 'done': 'undone')}>
               <input type="checkbox" checked={value.isDone}
                 onChange={(e) => this.updateTodoStatus(e, index)} />
               <span>{value.task}</span>
+              <a href="#" className="btn-delete-task" onClick={(e) => this.handleDelete(e, index)}><i className="fas fa-times"></i></a>
             </li>
           )}
         </ul>
       </div>
     );
+  }
+
+  handleDelete(e, index) {
+    e.preventDefault();
+    const tasks = [...this.state.tasks];
+    tasks.splice(index, 1);
+    this.setState({ tasks: tasks });
   }
 
   handleSubmit(e) {
@@ -40,14 +51,10 @@ export class App extends Component {
     this.setState({ tasks: tasks });
     e.target.reset();
   }
-
   updateTodoStatus(e, index) {
-    // console.log(index + ". eleman değişti. Yeni seçililik durumu: " + e.target.checked);
     const newTodos = [...this.state.tasks]; // mevcut dizinin kopyası
     newTodos[index].isDone = e.target.checked;
     this.setState({ task: newTodos });
   }
 }
-
 export default App;
-
